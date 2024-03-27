c829f6396dff90ab9c381fca6441808a54bcaa1f
redpath2312 <predpath2000@yahoo.co.uk>
2024-03-27 08:16:56 +0000

    Refactor with removing Base64 format for text in cat/says post request

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/" , async (req,res) => {
 try {
 
-
-    // *************
-    // OLD METHOD OF CONVERTING TO BASE 64
-    // const response = await axios.get(API_URL + "/cat?height=650", {responseType: 'arraybuffer'}); - old code to get base64 image.
-    // const buffer64 = Buffer.from(response.data, 'binary').toString('base64');
-    // The above converts the image from URL into base64.
-    // res.render("index.ejs", {cat: buffer64});
-    // *************
-
     const response = await axios.get(API_URL + "/cat?&json=true");
     // "/cat" will get a random cat but can specify height of image if adding query param of "/cat?height=" 
     // However to ensure this is consistent it is set for you here at 650. The css .container size and other queries should be set the same too.
@@ -54,24 +45,26 @@ app.get("/gif" , async (req,res) => {
     );
 
     
-    //Note couldn't get the text to work without using old method of base64 conversion.
   app.post("/says" , async (req,res) => {
     // newText is the form data user enters on the site for custom text. 
     const newText = req.body.catText;
     console.log(newText);
     try {
-        const response = await axios.get(API_URL + "/cat/says/" + newText + "?fontColor=white&fontSize=50", {responseType: 'arraybuffer'});
-        console.log("Attempted get")
+
+        const response = await axios.get(API_URL + "/cat/says/" + newText + "?&json=true");
+        const catID = response.data._id;
+        console.log(catID);
+        const catURL = API_URL + "/cat/" + catID + "/says/" + newText + "?fontColor=white&fontSize=50";
+        res.render("index.ejs", {catURL});
+        
         // To just perform a basic query through postman you could do https://cataas.com/cat/says/hello and it would retrieve an image with "hello" as the text. 
         // Additional query paramaters added here for text colour/font.
-        const buffer64 = Buffer.from(response.data, 'binary').toString('base64');
-        // The above converts the image from URL into base64.
-        res.render("index.ejs", {cat64: buffer64});
+        
     }
     catch (error) {
       console.error("Failed to make request:", error.message);
-      res.render("index.ejs", {error: error.message + ": Image to composite must have same dimensions or smaller",});
-      // Note - smaller height requests will result in more errors (this can be seen in postman) but 650 is around the maximum that will fit on standard screen. If setting height to about 500 or below here, it will nearly always error. This might not be seen now that the height is set in the ejs view and not initial request.
+      res.render("index.ejs", {error: "CatAPI failed with error: " + "'" + error.message +"'"});
+      // Note - smaller height requests through query params will result in more errors (can be seen in postman) if retrieving smaller sizes directly, but by not specifiying height and setting height afterwards in index.ejs this fixes it. 650 is around the maximum that will fit on standard screen. If setting height to about 500 or below here through query params, it will nearly always error.
     }});
 
 
